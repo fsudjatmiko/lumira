0b471fb8b02c40bd161b82e11e672c4c105c989a
frailled <fsudjatmiko@icloud.com>
2024-11-28 10:11:17 +0700

    after training

--- src/training.py
+++ src/training.py
@@ -19,8 +19,8 @@ np.random.seed(SEED)
 torch.use_deterministic_algorithms(True)
 
 device = torch.device("mps")
-total_epochs = 64
-batch_size = 16
+total_epochs = 100  # Increase the number of epochs
+batch_size = 32  # Increase the batch size
 
 if __name__ == "__main__":
 
@@ -37,11 +37,13 @@ if __name__ == "__main__":
     metrics.activation = torch.nn.Softmax(1)
 
     model = SimpleCNN(14).to(device)  # 14 classes for computer parts
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)  # Increase learning rate
 
     training_augmentation = [
-        #transforms.RandomHorizontalFlip(),
-        #transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     ]
 
     validation_dataset = SimpleTorchDataset('./splitted/val')
